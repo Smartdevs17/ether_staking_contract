eb9b3cda1f51345d62d71eed6af505a12b5ec3d4
smartdev <smartdevs17@gmail.com>
2024-08-31 13:42:36 +0100

    update owner of the er20 contract balance

--- contracts/ERC20Staking.sol
+++ contracts/ERC20Staking.sol
@@ -11,34 +11,35 @@ contract ERC20Staking {
     }
 
     mapping(address => StakingInfo) public stakingInfo;
-    uint256 public rewardBalance;
     IERC20 public token;
+    address private owner;
+    uint256 public rewardBalance = token.balanceOf(owner);
 
     event StakingStarted(address indexed user, uint256 amount, uint256 stakingTime);
     event RewardsWithdrawn(address indexed user, uint256 amount, uint256 withdrawnTime);
 
     constructor(address _token) {
+        owner = msg.sender;
         token = IERC20(_token);
     }
 
-    function stakeToken(uint256 _stakingTime) public {
+    function stakeToken(uint256 _stakingTime, uint256 _amount) public {
         require(msg.sender != address(0), "Cannot stake from address 0");
         require(_stakingTime > 0, "Staking time must be greater than zero");
-        uint256 reward = calculateReward(_stakingTime);
+        uint256 reward = calculateReward(_stakingTime, _amount);
         require(rewardBalance >= reward, "Insufficient reward balance");
         rewardBalance -= reward;
-        uint256 amount = token.balanceOf(msg.sender);
-        require(amount > 0, "Amount must be greater than zero");
-        require(amount <= token.balanceOf(msg.sender), "Amount cannot be greater than the user's balance");
-        token.transferFrom(msg.sender, address(this), amount);
-        stakingInfo[msg.sender] = StakingInfo(amount, block.timestamp + _stakingTime, reward);
+        require(_amount > 0, "Amount must be greater than zero");
+        require(_amount <= token.balanceOf(msg.sender), "Amount cannot be greater than the user's balance");
+        token.transferFrom(msg.sender, address(this), _amount);
+        stakingInfo[msg.sender] = StakingInfo(_amount, block.timestamp + _stakingTime, reward);
 
-        emit StakingStarted(msg.sender, amount, _stakingTime);
+        emit StakingStarted(msg.sender, _amount, _stakingTime);
     }
 
-    function calculateReward(uint256 _stakingTime) public pure returns (uint256) {
+    function calculateReward(uint256 _stakingTime, uint256 _amount) public pure returns (uint256) {
         uint256 secondsInAYear = 31536000; 
-        uint256 reward = _stakingTime / secondsInAYear;
+        uint256 reward = _amount * _stakingTime * 1e18 / secondsInAYear;
         return reward;
     }
 
@@ -51,6 +52,7 @@ contract ERC20Staking {
         stakingInfo[msg.sender].time = 0;
         stakingInfo[msg.sender].reward = 0;
         token.transfer(msg.sender, totalAmount);
+        
         emit RewardsWithdrawn(msg.sender, totalAmount, block.timestamp);
     }
 }
